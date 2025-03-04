eadcbb7c9ea6cd566e16701cc5a1cd18ec58e70c
charleswealthdev <charleswealthinternational@gmail.com>
2025-03-04 19:48:28 +0100

    character box fixed

--- Documents/Tech/Learnings/Three.js/demov2/src/main.js
+++ Documents/Tech/Learnings/Three.js/demov2/src/main.js
@@ -171,7 +171,7 @@ function onWindowResize() {
 const mainMenuScene = new THREE.Scene();
 const gameScene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 2, 6);
+camera.position.set(0, 2.3, 7);
 
 // Lighting
 const light = new THREE.AmbientLight(0xffffff, 1);
